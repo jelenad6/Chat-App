df87a0324c5f478e9976a6bbfc931182572a6def
Jelena <136815108+jelenad6@users.noreply.github.com>
2026-02-17 14:57:36 +0100

    Update PacketType.java

--- src/rs/raf/pds/v4/z5/messages/PacketType.java
+++ src/rs/raf/pds/v4/z5/messages/PacketType.java
@@ -1,8 +1,9 @@
 package rs.raf.pds.v4.z5.messages;
 
 public enum PacketType {
-    PUBLIC,     // broadcast svima (za sada)
+    PUBLIC,     // broadcast svima 
     DM,         // private poruka 1 korisniku
     MCAST,      // multicast grupi korisnika
     EDIT        // izmena postojece poruke
 }
+
